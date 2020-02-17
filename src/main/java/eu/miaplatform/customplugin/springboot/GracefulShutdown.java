72da4bd7a56303607c2fc8867c9dd78d371c7397
songle <songle.jin@mia-platform.eu>
2020-02-17 15:19:58 +0100

    added(new properties)

--- src/main/java/eu/miaplatform/customplugin/springboot/GracefulShutdown.java
+++ src/main/java/eu/miaplatform/customplugin/springboot/GracefulShutdown.java
@@ -2,19 +2,25 @@ package eu.miaplatform.customplugin.springboot;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.stereotype.Component;
 
+@Component
 public class GracefulShutdown implements ApplicationListener<ContextClosedEvent> {
 
     private static final Logger logger = LoggerFactory.getLogger(GracefulShutdown.class);
 
+    @Value("${delay.shutdown.seconds:0}")
+    private Long delayShutdownSeconds;
+
     @Override
     public void onApplicationEvent(ContextClosedEvent contextClosedEvent) {
         try {
-            logger.info("START GracefulShutdown...");
-            Thread.sleep(10000);
-            logger.info("COMPLETED GracefulShutdown");
+            logger.info("Start GracefulShutdown in {} seconds", delayShutdownSeconds);
+            Thread.sleep(delayShutdownSeconds * 1000);
+            logger.info("Completed GracefulShutdown");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
