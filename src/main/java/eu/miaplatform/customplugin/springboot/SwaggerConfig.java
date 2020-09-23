f1a27173a330241fecc15e37d44a119e82df2b35
Grieco <grieco@intre.it>
2020-09-23 15:48:32 +0200

    Update springboot library version

--- src/main/java/eu/miaplatform/customplugin/springboot/SwaggerConfig.java
+++ src/main/java/eu/miaplatform/customplugin/springboot/SwaggerConfig.java
@@ -63,4 +63,8 @@ public class SwaggerConfig extends CPSwaggerConfig {
         return "info@mia-platform.eu";
     }
 
+    @Override
+    public String getSelector() {
+        return "eu.miaplatform.customplugin.springboot";
+    }
 }
